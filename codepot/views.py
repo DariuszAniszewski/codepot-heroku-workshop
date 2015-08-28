7b352e607d92e3e90dc5c7f6dc2ff5973b99caa2
Dariusz Aniszewski <dariusz.aniszewski@polidea.com>
2015-08-28 19:33:12 +0200

    logs

--- codepot/views.py
+++ codepot/views.py
@@ -6,3 +6,11 @@ def home(request):
 
     }
     return render(request, "index.html", data)
+
+
+def log(request):
+    for i in range(2000):
+        print("Dummy log line #{}".format(i))
+        if i == 200:
+            print("WOW THIS IS SUPER IMPORTANT LINE WITH ERROR")
+    return render(request, "logs.html")
